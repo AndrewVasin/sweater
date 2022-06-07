1d825dd622d89ae566e01080ae3ba2871f85cd6b
AndrewVasin <ironfeyd@gmail.com>
2022-06-07 17:35:07 +0300

    Init commit

---
+++ src/main/java/com/example/sweater/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package com.example.sweater.repos;
+
+import com.example.sweater.domain.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends JpaRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
+}

7957d5f70f443ff7ad1c4b92a55f6cf1d6a90814
AndrewVasin <ironfeyd@gmail.com>
2022-06-08 00:36:24 +0300

    добавлена авторизация пользователя

---
+++ src/main/java/com/example/sweater/domain/User.java
@@ -0,0 +1,29 @@
+package com.example.sweater.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private Boolean active;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+
+    public User() {
+    }
+}
